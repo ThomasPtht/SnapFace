9bd3c2035b2e8b5afe0df2b074ba129d675f14c4
Thomas Potherat <t.potherat@laposte.net>
2026-01-05 15:07:32 +0100

    feat: refactor FaceSnap initialization and update template to use faceSnaps array

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,36 +10,42 @@ import { FaceSnap } from './models/face-snap';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
+
 export class App {
+  faceSnaps!: FaceSnap[];
+
   mySnap!: FaceSnap;
   myOtherSnap!: FaceSnap;
   myLastSnap!: FaceSnap;
 
   ngOnInit(): void {
-    this.mySnap = new FaceSnap(
-      "Archibald",
-      "Mon meilleur ami depuis tout petit !",
-      "https://victoiredavril.com/wp-content/uploads/2023/01/AC5368C9-F205-4EF4-8E80-292F2284F2F4-e1734105197826.webp",
-      new Date(),
-      10
-    );
-    this.myOtherSnap = new FaceSnap(
-      "Tokyo Skytree",
-      "Une tour de télévision japonaise à Tokyo.",
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_y4cKxwnwMCScsX254iVuRsPLwcUa_bgobg&s",
-      new Date(),
-      42
-    );
-    this.myLastSnap = new FaceSnap(
-      "Repas de Saint Valentin",
-      "Un repas romantique pour la Saint Valentin.",
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpggWs89k8NrbA4DGRvMV1g2EewFColWQ82A&s",
-      new Date(),
-      128
-    );
+    this.faceSnaps = [
+      new FaceSnap(
+        "Archibald",
+        "Mon meilleur ami depuis tout petit !",
+        "https://victoiredavril.com/wp-content/uploads/2023/01/AC5368C9-F205-4EF4-8E80-292F2284F2F4-e1734105197826.webp",
+        new Date(),
+        10
+      ),
+      new FaceSnap(
+        "Tokyo Skytree",
+        "Une tour de télévision japonaise à Tokyo.",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_y4cKxwnwMCScsX254iVuRsPLwcUa_bgobg&s",
+        new Date(),
+        42
+      ),
+      new FaceSnap(
+        "Repas de Saint Valentin",
+        "Un repas romantique pour la Saint Valentin.",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpggWs89k8NrbA4DGRvMV1g2EewFColWQ82A&s",
+        new Date(),
+        128
+      )
+    ];
 
-    this.myOtherSnap.setLocation("Tokyo, Japan");
-    
+
+    this.faceSnaps[1].setLocation("Tokyo, Japan");
+
   }
 
 }
