233ff2ef29e0717cddb44c70009fab0ac6b046f6
Thomas Potherat <t.potherat@laposte.net>
2026-01-08 09:54:03 +0100

    feat: add interval observable and display in app template

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,22 +2,26 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FaceSnap } from './models/face-snap';
 import { Header } from './header/header';
+import { interval, Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [Header, RouterOutlet],
+  imports: [Header, RouterOutlet, AsyncPipe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 
 export class App {
 
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
 
+  interval$!: Observable<number>;
+
+  ngOnInit() {
+    this.interval$ = interval(1000);
+  }
 
 
 }
