7bba0aabf8979c46fc86a9c71df31a9b9bc0e5c0
Thomas Potherat <t.potherat@laposte.net>
2026-01-08 10:57:54 +0100

    feat: testing high level observables

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
-import { interval, Observable } from 'rxjs';
+import { interval, of } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
-import { filter, map, tap } from 'rxjs/operators';
+import { concatMap, mergeMap, delay, exhaustMap, map, switchMap, take, tap } from 'rxjs/operators';
 
 
 
@@ -16,17 +16,30 @@ import { filter, map, tap } from 'rxjs/operators';
 
 export class App {
 
-
-  interval$!: Observable<string>;
+  redTrainsCalled = 0;
+  yellowTrainsCalled = 0;
 
   ngOnInit() {
-    this.interval$ = interval(1000).pipe(filter(value => value % 3 === 0), map(value => value % 2 === 0 ? `Je suis ${value}, je suis pair !` : `Je suis ${value}, je suis impair !`),
-      tap(text => this.logger(text))
-    )
+    interval(500).pipe(
+      take(10),
+      map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
+      tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
+      exhaustMap(color => this.getTrainObservable$(color)),
+      tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
+    ).subscribe();
   }
 
-  logger(text: string) {
-    console.log(`Log: ${text}`);
+  getTrainObservable$(color: 'rouge' | 'jaune') {
+    const isRedTrain = color === 'rouge';
+    isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
+    const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
+    console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
+    return of({ color, trainIndex }).pipe(
+      delay(isRedTrain ? 5000 : 6000)
+    );
   }
 
+  translateColor(color: 'rouge' | 'jaune') {
+    return color === 'rouge' ? 'red' : 'yellow';
+  }
 }
