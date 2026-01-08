b99cb01f5c535a4dab3cf1a3fc3bb7366de1b768
Thomas Potherat <t.potherat@laposte.net>
2026-01-08 11:07:53 +0100

    feat: first setup to avoid memory leak with take

--- src/app/face-snap-list/face-snap-list.ts
+++ src/app/face-snap-list/face-snap-list.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap';
 import { FaceSnapComponent } from '../face-snap/face-snap';
 import { FaceSnapService } from '../services/face-snaps.service';
+import { interval, tap, take } from 'rxjs';
 
 @Component({
   selector: 'app-face-snap-list',
@@ -16,8 +17,12 @@ export class FaceSnapList implements OnInit {
 
   ngOnInit(): void {
     this.faceSnaps = this.faceSnapsService.getFaceSnaps()
- 
 
+    interval(1000).pipe(
+      take(5),
+      tap(value => console.log(value)
+      )
+    ).subscribe();
   }
 
 }
